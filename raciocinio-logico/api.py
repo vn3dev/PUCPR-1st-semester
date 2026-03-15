f15fb70634c730c709a0abf4cab5122913072cc9
Vinicius Costa <vinicosta.vn3dev@gmail.com>
2026-03-14 22:48:22 -0300

    upload math and algorithm files

---
+++ raciocinio-logico/api.py
@@ -0,0 +1,35 @@
+from flask import Flask, json, jsonify, request
+
+app = Flask(__name__)
+app.json.sort_keys = False
+
+@app.get("/listar/pacientes")
+def get_paciente():
+    with open('pacientes.json', 'r', encoding="utf-8") as listaDoador:
+        resposta = json.load(listaDoador)
+    
+    return jsonify(resposta)
+
+@app.get("/listar/sangue")
+def get_sangue():
+    with open('sangue.json', 'r') as listaSangue:
+        resposta = json.load(listaSangue)
+
+    return jsonify(resposta)
+
+@app.post("/adicionar")
+def add_doador():
+
+    novo_doador = request.json
+
+    with open('pacientes.json', 'r', encoding="utf-8") as listaDoador:
+        doadores = json.load(listaDoador)
+
+    doadores.append(novo_doador)
+
+    with open('pacientes.json', 'w', encoding="utf-8") as listaDoador:
+        json.dump(doadores, listaDoador, indent=4, ensure_ascii=False)
+
+    return jsonify(novo_doador), 201    
+    
+app.run(debug = True)
---
+++ raciocinio-logico/docrotas.md
@@ -0,0 +1,4 @@
+Documento de Rotas:
+
+GET listar_pacientes
+GET listar_estoque_sangue
